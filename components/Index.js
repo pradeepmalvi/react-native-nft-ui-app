931870babefa7c90a7526f87047ad349c513b229
pradeepmalvi <p.malviyaaa@gmail.com>
2022-12-16 18:17:34 +0530

    NFT card UI list updates

---
+++ components/Index.js
@@ -0,0 +1,6 @@
+import FocusStatusBar from "./FocusStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./CircleButton";
+
+export { HomeHeader, FocusStatusBar, NFTCard, CircleButton, RectButton };
