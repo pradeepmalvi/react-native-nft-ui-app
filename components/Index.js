c80436af898fd0a29d3715ef179686f4b379eff8
pradeepmalvi <p.malviyaaa@gmail.com>
2022-12-18 18:40:53 +0530

    Details page UI completed

--- components/Index.js
+++ components/Index.js
@@ -2,5 +2,18 @@ import FocusStatusBar from "./FocusStatusBar";
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./CircleButton";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo } from "./SubInfo";
 
-export { HomeHeader, FocusStatusBar, NFTCard, CircleButton, RectButton };
+
+export {
+  HomeHeader,
+  FocusStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo
+};
